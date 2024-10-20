6c3dffdd4454c9415bc6a709dbecb8b238e9b5aa
Aarav Sharma <iamcoder18@gmail.com>
2024-10-20 16:54:32 -0600

    hang code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Slide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Slide.java
@@ -36,8 +36,8 @@ public class Slide {
         slide_right.setTargetPosition(slide_right.getCurrentPosition());
         slide_left.setPower(Constants.slideBrakeSpeed);
         slide_right.setPower(Constants.slideBrakeSpeed);
-        slide_left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        slide_right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slide_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slide_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
 
