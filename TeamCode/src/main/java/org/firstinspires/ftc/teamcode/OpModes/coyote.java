4851433ab8da7c93ccc79840a6ecd86401b9da94
biyo-u <biyouvbiama@gmail.com>
2024-10-27 13:50:24 -0600

    added import statements to coyote.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/coyote.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/coyote.java
@@ -1,4 +1,39 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-public class coyote {
+import androidx.annotation.NonNull;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+// Subsystem Imports
+import org.firstinspires.ftc.teamcode.Subsystems.Slide;
+import org.firstinspires.ftc.teamcode.Subsystems.Drive;
+import org.firstinspires.ftc.teamcode.Subsystems.Odometry;
+
+@Autonomous (name = "coyote v0.0.1", group = "TECH TITANS 2025")
+public class coyote extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
+
